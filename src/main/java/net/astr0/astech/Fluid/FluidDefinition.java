f47427930dafeb8736c9c0c30fbdfcb8fbce4a20
Angus Barnes <38348730+angusbarnes@users.noreply.github.com>
2024-05-24 20:39:20 +1000

    Added Some Fluid Helpers

---
+++ src/main/java/net/astr0/astech/Fluid/FluidDefinition.java
@@ -0,0 +1,6 @@
+package net.astr0.astech.Fluid;
+
+// Maybe have class which keeps track of the flowing block, the source block
+// the fluid type and the bucket item for each fluid.
+public class FluidDefinition {
+}
---
+++ src/main/java/net/astr0/astech/Fluid/FluidDefinitions.java
@@ -0,0 +1,12 @@
+package net.astr0.astech.Fluid;
+
+import java.util.List;
+
+public class FluidDefinitions {
+
+    public static List<FluidDefinition> Fluids = List.of(
+            new FluidDefinition(),
+            new FluidDefinition(),
+            new FluidDefinition()
+    );
+}

10daa779eaa15c5294cab8b41f6b540587940194
Angus Barnes <38348730+angusbarnes@users.noreply.github.com>
2024-06-02 19:22:15 +1000

    Added base class for network manager

---
+++ src/main/java/net/astr0/astech/network/AsTechNetworkHandler.java
@@ -0,0 +1,22 @@
+package net.astr0.astech.network;
+
+import net.astr0.astech.AsTech;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraftforge.network.NetworkRegistry;
+import net.minecraftforge.network.simple.SimpleChannel;
+
+public class AsTechNetworkHandler {
+
+    private static final String PROTOCOL_VERSION = "1";
+    public static final SimpleChannel INSTANCE = NetworkRegistry.newSimpleChannel(
+            new ResourceLocation(AsTech.MODID, "main"),
+            () -> PROTOCOL_VERSION,
+            PROTOCOL_VERSION::equals,
+            PROTOCOL_VERSION::equals
+    );
+
+    public static void onCommonSetup() {
+        INSTANCE.registerMessage(0, FilterUpdateMessage.class, FilterUpdateMessage::encode, FilterUpdateMessage::decode, FilterUpdateMessage::handle);
+    }
+
+}

439b335283a9f5ffd62c3efcc49c6f9cad07c077
Angus Barnes <38348730+angusbarnes@users.noreply.github.com>
2024-09-25 04:40:03 +0000

    Added template for hazard effects

--- src/main/java/net/astr0/astech/item/AsTechBucketItem.java
+++ src/main/java/net/astr0/astech/item/AsTechBucketItem.java
@@ -11,6 +11,8 @@ import net.minecraft.tags.FluidTags;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.*;
 import net.minecraft.world.level.Level;
@@ -75,6 +77,9 @@ public class AsTechBucketItem extends BucketItem {
                 if (entity instanceof Player player) {
                     player.hurt(source, 200f);
                 }
+
+                // Modify this as needed for hazardous materials
+                ((LivingEntity) entity).addEffect(new MobEffectInstance(MobEffects.POISON, 200), pAttacker);
             }
             tag.putInt("danger_ttl", count - 1);
 

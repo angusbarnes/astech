8d718b7e4219b682c85c6d57fb9b755379863bdc
Angus Barnes <38348730+angusbarnes@users.noreply.github.com>
2025-06-08 18:30:25 +1000

    Allow JEI dragged ingredients to set filters in BEs

--- src/main/java/net/astr0/astech/compat/JEI/GhostIngredientHandler.java
+++ src/main/java/net/astr0/astech/compat/JEI/GhostIngredientHandler.java
@@ -1,11 +1,15 @@
 package net.astr0.astech.compat.JEI;
 
+import com.mojang.logging.LogUtils;
+import mezz.jei.api.constants.VanillaTypes;
+import mezz.jei.api.forge.ForgeTypes;
 import mezz.jei.api.gui.handlers.IGhostIngredientHandler;
 import mezz.jei.api.ingredients.ITypedIngredient;
 import net.astr0.astech.gui.AbstractGuiSlot;
 import net.astr0.astech.gui.AsTechGuiElement;
 import net.astr0.astech.gui.AsTechGuiScreen;
 import net.minecraft.client.renderer.Rect2i;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.fluids.FluidStack;
 import org.jetbrains.annotations.NotNull;
@@ -13,7 +17,7 @@ import org.jetbrains.annotations.Nullable;
 
 import java.util.ArrayList;
 import java.util.List;
-
+import java.util.Optional;
 
 
 public class GhostIngredientHandler implements IGhostIngredientHandler<AsTechGuiScreen> {
@@ -23,10 +27,12 @@ public class GhostIngredientHandler implements IGhostIngredientHandler<AsTechGui
     public <I> @NotNull List<Target<I>> getTargetsTyped(AsTechGuiScreen gui, ITypedIngredient<I> ingredient, boolean doStart) {
         List<Target<I>> targets = new ArrayList<>();
 
+        DraggedIngredient ing = wrapDraggedItem(ingredient.getIngredient());
+
         List<AsTechGuiElement> elements = gui.getGuiElements();
         for(AsTechGuiElement element : elements) {
 
-            if (element instanceof AbstractGuiSlot guiSlot) {
+            if (element instanceof AbstractGuiSlot guiSlot && guiSlot.canAcceptGhostIngredient(ing)) {
                 Rect2i area = guiSlot.getRect();
 
                 targets.add(new Target<I>() {
@@ -38,10 +44,8 @@ public class GhostIngredientHandler implements IGhostIngredientHandler<AsTechGui
                     @Override
                     public void accept(I ingredient) {
                         System.out.println("Ghost ingredient dropped: " + ingredient);
-
-                        DraggedIngredient result = wrapDraggedItem(ingredient);
-
-                        guiSlot.handleFilterDrop(result);
+                        DraggedIngredient ing = wrapDraggedItem(ingredient);
+                        guiSlot.handleFilterDrop(ing);
                     }
                 });
             }
@@ -53,11 +57,26 @@ public class GhostIngredientHandler implements IGhostIngredientHandler<AsTechGui
 
     @Nullable
     private static DraggedIngredient wrapDraggedItem(Object ingredient) {
+        Optional<ItemStack> maybeItem = VanillaTypes.ITEM_STACK.castIngredient(ingredient);
+        if (maybeItem.isPresent()) {
+            return new DraggedIngredient.Item(maybeItem.get());
+        }
+
+        Optional<FluidStack> maybeFluid = ForgeTypes.FLUID_STACK.castIngredient(ingredient);
+        if (maybeFluid.isPresent()) {
+            return new DraggedIngredient.Fluid(maybeFluid.get());
+        }
+
+        // Fallbacks for raw types if not provided by castIngredient
         if (ingredient instanceof ItemStack stack) {
             return new DraggedIngredient.Item(stack);
+        } else if (ingredient instanceof Item item) {
+            return new DraggedIngredient.Item(new ItemStack(item));
         } else if (ingredient instanceof FluidStack fluid) {
             return new DraggedIngredient.Fluid(fluid);
         }
+
+        LogUtils.getLogger().info("Unhandled dragged ingredient: {}", ingredient);
         return null;
     }
 

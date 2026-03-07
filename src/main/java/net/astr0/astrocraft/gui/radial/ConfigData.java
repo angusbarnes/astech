fc1aaa4a90acfb5af1c6f6839f3afb6beac321ba
Angus Barnes <38348730+angusbarnes@users.noreply.github.com>
2026-03-07 22:18:45 +1100

    Condemn old radial menu system

--- src/main/java/net/astr0/astrocraft/gui/radial/ConfigData.java
+++
@@ -1,6 +0,0 @@
-package net.astr0.astrocraft.gui.radial;
-
-public class ConfigData {
-    public static final boolean clipMouseToCircle = true;
-    public static final boolean allowClickOutsideBounds = true;
-}
